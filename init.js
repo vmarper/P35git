03cbcff2fe0da04eb6470e2ff5289f1eca4784f1
Veronica <vmarper98@gmail.com>
2021-09-03 21:12:53 -0500

    Estado 1: creación de tabla

---
+++ init.js
@@ -0,0 +1,11 @@
+let nombre = "Marina"
+        let edad= 46;
+        let estatura =1.5;
+        let casado = true;
+        document.writeln(nombre);
+        document.writeln("<br>")
+        document.writeln(edad);
+        let profesion = prompt("Digita tu profesión");
+        alert("Tu profesión es" + profesion)
+        let nom = document.getElementById(t1);
+        document.getElementById("t1").innerHTML=nombre
